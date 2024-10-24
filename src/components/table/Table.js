509c9db39d184663c2e2b222c7cc2d7a64d7e7e6
Tinatin <tinaytini@yahoo.com>
2024-10-24 15:21:35 +0200

    delegated functions into another file

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import { DomListener } from "../../core/DomListener";
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
 import { $ } from "../../core/Dom";
+import { resizeHandler } from "./table.resize"
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -19,53 +20,9 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         const type = event.target.dataset.resize
-        if (type) {
-            const $resizer = $(event.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            let value;
-            const cells = this.$root.findAll(`[data-col="${$parent.$el.dataset.col}"]`)
-            const sideProp = type === 'col' ? 'bottom' : 'right'
-            $resizer.css({
-                opacity: 1,
-                zIndex: 1000,
-                [sideProp]: '-5000px'
-            })
-            
-            
-            document.onmousemove = e => {
-                if (type === 'col') {
-                    const deltaCol = e.pageX - coords.$el.right
-                    value = coords.$el.width + deltaCol
-                    $resizer.css({right: -deltaCol + 'px'})
-                } else {
-                    const deltaRow = e.pageY - coords.$el.bottom
-                    value = coords.$el.height + deltaRow
-                    $resizer.css({bottom: -deltaRow + 'px'})
-                }
-
-                document.onmouseup = (e) => {
-                    if (type === 'col') {
-                        $parent.css({width: value + 'px'})
-                        cells.forEach(el => {
-                            el.style.width = value + 'px';
-                        })
-                    } 
-                    if (type === 'row') {
-                        $parent.css({height: value + 'px'})
-                    }
-                    document.onmousemove = null
-                    document.onmouseup = null
-                    $resizer.css({
-                        opacity: 0,
-                        bottom: 0,
-                        right: 0,
-                    })
-                    
-                }
-            }
-        }
-
+        if (type) { //should resize
+            resizeHandler(this.$root, event, type)
+        } 
     }
 
 } 
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,48 @@
+import { $ } from "../../core/Dom";
+
+export function resizeHandler($root, event, type) {
+    const $resizer = $(event.target)
+    const $parent = $resizer.closest('[data-type="resizable"]')
+    const coords = $parent.getCoords()
+    let value;
+    const cells = $root.findAll(`[data-col="${$parent.$el.dataset.col}"]`)
+    const sideProp = type === 'col' ? 'bottom' : 'right'
+    $resizer.css({
+        opacity: 1,
+        zIndex: 1000,
+        [sideProp]: '-5000px'
+    })
+
+
+    document.onmousemove = e => {
+        if (type === 'col') {
+            const deltaCol = e.pageX - coords.$el.right
+            value = coords.$el.width + deltaCol
+            $resizer.css({ right: -deltaCol + 'px' })
+        } else {
+            const deltaRow = e.pageY - coords.$el.bottom
+            value = coords.$el.height + deltaRow
+            $resizer.css({ bottom: -deltaRow + 'px' })
+        }
+
+        document.onmouseup = (e) => {
+            if (type === 'col') {
+                $parent.css({ width: value + 'px' })
+                cells.forEach(el => {
+                    el.style.width = value + 'px';
+                })
+            }
+            if (type === 'row') {
+                $parent.css({ height: value + 'px' })
+            }
+            document.onmousemove = null
+            document.onmouseup = null
+            $resizer.css({
+                opacity: 0,
+                bottom: 0,
+                right: 0,
+            })
+
+        }
+    }
+}
